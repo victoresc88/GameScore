c6a91f4904848668fa5bf893dea0436f677cbae8
Victor <vicesc88@gmail.com>
2020-03-21 19:02:17 +0100

    Add dependencies missing

---
+++ GameScore.EntityFramework.DAL/GameScoreDbContextFactory.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace GameScore.EntityFramework.DAL
+{
+	public class GameScoreDbContextFactory : IDesignTimeDbContextFactory<GameScoreDbContext>
+	{
+		private static string _connectionString;
+
+		public GameScoreDbContext CreateDbContext()
+		{
+			return CreateDbContext(null);
+		}
+
+		public GameScoreDbContext CreateDbContext(string[] args)
+		{
+			if (string.IsNullOrEmpty(_connectionString))
+			{
+				LoadConnectionString();
+			}
+
+			var builder = new DbContextOptionsBuilder<GameScoreDbContext>();
+			builder.UseSqlServer(_connectionString);
+
+			return new GameScoreDbContext(builder.Options);
+		}
+
+		private static void LoadConnectionString()
+		{
+			var builder = new ConfigurationBuilder();
+			builder.AddJsonFile("appsettings.json", optional: false);
+
+			var configuration = builder.Build();
+
+			_connectionString = configuration.GetConnectionString("DefaultConnection");
+		}
+	}
+}
