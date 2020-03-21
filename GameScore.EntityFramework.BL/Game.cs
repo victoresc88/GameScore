89679aa205909867d623d68e1ff228600a17faad
Victor <vicesc88@gmail.com>
2020-03-21 18:04:06 +0100

    Add EF Business Layer

---
+++ GameScore.EntityFramework.BL/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameScoreFetchDataJob.Models
+{
+    [Table("Games")]
+    public class Game
+    {
+        public int Id { get; set; }
+        public int OriginalId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int PlayTime { get; set; }
+        public int Metacritic { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ GameScore.EntityFramework.BL/GenreGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameScoreFetchDataJob.Models
+{
+	public class GenreGame
+	{
+		public int GameId { get; set; }
+		public Game Game { get; set; }
+		public int GenreId { get; set; }
+		public Genre Genre { get; set; }
+
+	}
+}
