d7b27c4cb45e65a711f8993a14d0b545a8c96f7e
Victor <victor@yukisoftware.es>
2020-05-16 19:33:42 +0200

    Add rating component

---
+++ GameScore.UI/ViewComponents/RateViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using GameScore.BL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameScore.UI.ViewComponents
+{
+	public class RateViewComponent : ViewComponent
+	{
+		private readonly IScoreBusiness _scoreBusiness;
+
+		public RateViewComponent(IScoreBusiness scoreBusiness)
+		{
+			_scoreBusiness = scoreBusiness;
+		}
+
+		public IViewComponentResult Invoke()
+		{
+			return View("Rate");
+		}
+	}
+}
--- GameScore.UI/ViewModels/GameDetailsViewModel.cs
+++ GameScore.UI/ViewModels/GameDetailsViewModel.cs
@@ -6,15 +6,14 @@ using System.Threading.Tasks;
 
 namespace GameScore.UI.ViewModels
 {
-    public class GameDetailsViewModel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string ReleaseDate { get; set; }
-        public int PlayTime { get; set; }
-        public int Metacritic { get; set; }
-        public string ImageUrl { get; set; }
-        public Score Score { get; set; }
-    }
+	public class GameDetailsViewModel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string ReleaseDate { get; set; }
+		public int PlayTime { get; set; }
+		public int Metacritic { get; set; }
+		public string ImageUrl { get; set; }
+	}
 }
